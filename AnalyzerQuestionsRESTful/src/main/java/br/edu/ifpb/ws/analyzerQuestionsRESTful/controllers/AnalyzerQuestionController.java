54aa81deb41881f415fa3f773e2eb2704d29ed04
Franck Aragão <fjunior.aragao@gmail.com>
2016-10-18 01:12:11 -0300

    rest ok

--- AnalyzerQuestionsRESTful/src/main/java/br/edu/ifpb/ws/analyzerQuestionsRESTful/controllers/AnalyzerQuestionController.java
+++ AnalyzerQuestionsRESTful/src/main/java/br/edu/ifpb/ws/analyzerQuestionsRESTful/controllers/AnalyzerQuestionController.java
@@ -1,8 +1,17 @@
 package br.edu.ifpb.ws.analyzerQuestionsRESTful.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifpb.ws.analyzerQuestionsRESTful.entities.Question;
+import br.edu.ifpb.ws.analyzerQuestionsRESTful.services.AnalyzerQuestionService;
 
 /**
  * 
@@ -15,8 +24,17 @@ public class AnalyzerQuestionController {
 	
 	public static final String BASE_URI = "/analyzer";
 	
-	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public String analyzerQuestion(){
-		return "hello world ws!";
+	private AnalyzerQuestionService service;
+
+	public AnalyzerQuestionController() {
+		service = new AnalyzerQuestionService();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<String>> getSuggestions(@RequestBody Question question){
+		
+		List<String> suggestions = service.getSuggestions(question);
+		
+		return new ResponseEntity<List<String>>(suggestions, HttpStatus.OK);
 	}
 }
