76763a91624c65b58572b843702da2919c9a7efe
Franck Aragão <fjunior.aragao@gmail.com>
2016-10-20 12:07:15 -0300

    controle de uma nova pergunta

--- AnalyzerQuestionsRESTful/src/main/resources/static/js/controllers/new-questions-controller.js
+++ AnalyzerQuestionsRESTful/src/main/resources/static/js/controllers/new-questions-controller.js
@@ -1,7 +1,16 @@
+/**
+ * Controller responsável pela manipulação do fragmento de  página new-question.html
+ * 
+ * @created by Franck Aragão 20-10-16.
+ */
 aqtApp.controller('newQuestionController', function($scope, $http) {
 	
 	$scope.suggestions = [];
+	var chosenSuggestions = [];
 
+	/**
+	 * Submete uma nova pergunta à API.
+	 */
 	$scope.newQuestion = function(question) {
 		
 		post = $('#editor-f').data('markdown').parseContent();
@@ -14,21 +23,25 @@ aqtApp.controller('newQuestionController', function($scope, $http) {
 
 		}).then(function onSucces(response) {
 			$scope.suggestions = response.data;
-			console.log($scope.suggestions);
 			
 		}, function onError(response) {
-			
 		});
 	};
 	
+	/**
+	 * Controla as escolhas de sugestões feitas pelo user.
+	 */
 	$scope.checkedSuggestion = function(suggestion, isChecked){
 		var index = $scope.suggestions.indexOf(suggestion);
 		var myEl = $(".aqt-close").eq(index);
 		if(isChecked){
 			myEl.addClass('alert-success'); 
+			chosenSuggestions.push(suggestion);
 		}
 		else{
 			myEl.removeClass('alert-success');
+			var indexChoose = chosenSuggestions.indexOf(suggestion);
+			chosenSuggestions.splice(indexChoose, 1);
 		}
 	};
 });
