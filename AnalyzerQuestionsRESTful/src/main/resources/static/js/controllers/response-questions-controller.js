ce53db6069b4bc1961738267eab812a486ff6260
Franck Aragão <fjunior.aragao@gmail.com>
2016-10-25 14:04:11 -0300

    anti-padrão

--- AnalyzerQuestionsRESTful/src/main/resources/static/js/controllers/response-questions-controller.js
+++ AnalyzerQuestionsRESTful/src/main/resources/static/js/controllers/response-questions-controller.js
@@ -1,14 +1,18 @@
 /**
- * Controller responsável pela manipulação do fragmento de  página list-questions.html
+ * Controller responsável pela manipulação do fragmento de página
+ * list-questions.html
  * 
- * @created  @date 21-10-16.
+ * @created
+ * @date 21-10-16.
  */
-aqtApp.controller("responseQuestionController", function($scope, $http, $location) {
-	
-	const URI = 'http://localhost:8080';
+aqtApp.controller("responseQuestionController", function($scope, $http,
+		$location) {
+
+	const
+	URI = 'http://localhost:8080';
 	var chosenQuestions = [];
 	$scope.question = {};
-	
+
 	/**
 	 * Obtém lista de perguntas ecolhidas do WS.
 	 */
@@ -20,35 +24,22 @@ aqtApp.controller("responseQuestionController", function($scope, $http, $locatio
 
 		}).then(function onSuccess(response) {
 			chosenQuestions = response.data;
-			$scope.nextQuestion();
+			$scope.nextQuestion.next();
 
 		}, function onError(response) {
 
 		});
 	};
-	
-	
-	$scope.nextQuestion = function() {
-		var cont = 0;
-		
-		$scope.question = chosenQuestions[cont];
-		var elemetQuestion = $("#body-detail-description");
-		elemetQuestion.append($scope.question.descritptionHtml);
-		
-		var next = function(){
-			cont++;
-			console.log(cont);
-			$scope.question = chosenQuestions[cont];
+
+	$scope.nextQuestion = {
+		cont : 0,
+		next : function() {
+			$scope.question = chosenQuestions[this.cont];
 			var elemetQuestion = $("#body-detail-description");
 			elemetQuestion.append($scope.question.descritptionHtml);
+			this.valor++;
 		}
-		
-		return {
-			next:next
-		}
-	};
-	
+	}
 
 	$scope.getSelectedQuestions();
-	
 });
