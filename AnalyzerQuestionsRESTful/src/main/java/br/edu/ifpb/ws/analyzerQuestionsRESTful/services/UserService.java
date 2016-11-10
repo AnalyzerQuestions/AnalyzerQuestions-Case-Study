537a30c76c3fa17a40a4328464b85a3fe273434b
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2016-11-10 11:30:52 -0300

    adicionado pontos para registro e atualização de user

---
+++ AnalyzerQuestionsRESTful/src/main/java/br/edu/ifpb/ws/analyzerQuestionsRESTful/services/UserService.java
@@ -0,0 +1,47 @@
+package br.edu.ifpb.ws.analyzerQuestionsRESTful.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.edu.ifpb.ws.analyzerQuestionsRESTful.entities.User;
+import br.edu.ifpb.ws.analyzerQuestionsRESTful.repository.UserRepository;
+
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	/**
+	 * Método responsável por salvar um usário no banco de dados
+	 * @param user
+	 * @return
+	 */
+	public User saveUser(User user){
+		
+		this.userRepository.save(user);
+		
+		return user;
+	}
+	
+	
+	/**
+	 * Método responsável por buscar um usário com id passado no banco de dados
+	 * @param id
+	 * @return
+	 */
+	public User findUserById(Long id){
+		
+		return this.userRepository.findOne(id);
+	}
+	
+	/**
+	 * Método responsável por atualizar as informações de um usário no banco de dados
+	 * @param user
+	 * @return
+	 */
+	public User updateUser(User user){
+		this.userRepository.save(user);
+		return user;
+	}
+
+}
