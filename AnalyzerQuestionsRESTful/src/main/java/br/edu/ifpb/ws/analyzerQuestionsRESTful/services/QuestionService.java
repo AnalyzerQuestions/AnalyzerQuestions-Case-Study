1ebd2e2c5e955ebd9a7e56af82ffd95f68c64d04
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2016-11-15 13:20:53 -0300

    adicionado método para calcular porcetagem

--- AnalyzerQuestionsRESTful/src/main/java/br/edu/ifpb/ws/analyzerQuestionsRESTful/services/QuestionService.java
+++ AnalyzerQuestionsRESTful/src/main/java/br/edu/ifpb/ws/analyzerQuestionsRESTful/services/QuestionService.java
@@ -4,28 +4,62 @@ import java.util.List;
 
 import br.edu.ifpb.ws.analyzerQuestionsRESTful.entities.Question;
 import br.edu.ifpb.ws.analyzerQuestionsRESTful.util.data.ReaderQuestions;
+import opennlp.maxent.Main;
 
 /**
  * 
- * @author <a href="https://github.com/FranckAJ">Franck Aragão</a>	
+ * @author <a href="https://github.com/FranckAJ">Franck Aragão</a>
  *
  */
 public class QuestionService {
-	
-	private AnalyzerQuestion analyzer;
-	
+
+	private AnalyzerQuestionSuggestion analyzer;
+	private AnalyzerQuestion analyzerQuestion;
+
 	public QuestionService() {
-		analyzer = new AnalyzerQuestion();
+		analyzer = new AnalyzerQuestionSuggestion();
+		analyzerQuestion = new AnalyzerQuestion();
 	}
-	
+
 	public List<Question> getQuestions() {
 		ReaderQuestions rq = new ReaderQuestions("perguntas.csv");
 		List<Question> questions = rq.readCsvFile();
-		
+
+		for (Question q : questions) {
+			
+		}
+
 		return questions;
 	}
 	
-	public List<String> getAnalize(Question question){
+	public Float goodQuestionAverage(){
+		ReaderQuestions rq = new ReaderQuestions("perguntas.csv");
+		List<Question> questions = rq.readCsvFile();
+
+		int cont = 0;
+		for (Question q : questions) {
+			if(goodQuestion(q)==1){
+				cont++;
+			}
+		}
+
+		System.out.println(questions.size());
+		Float average = (float) ((cont*100)/questions.size());
+		return average;
+	}
+
+	private Integer goodQuestion(Question question) {
+		return analyzerQuestion.getGoodQuestionAnalyzer(question);
+	}
+
+	public List<String> getAnalize(Question question) {
 		return analyzer.getSuggestions(question);
+	}
+	
+	
+	public static void main(String[] args) {
+		QuestionService qu = new QuestionService();
+		
+		System.out.println(qu.goodQuestionAverage());
 	}
 }
