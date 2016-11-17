f26caabd820a1cc56b91afadec7513c3d9cc61f4
Franck Aragão <fjunior.aragao@gmail.com>
2016-11-17 11:01:29 -0300

    avançando e voltando para anterior

--- AnalyzerQuestionsRESTful/src/main/resources/static/js/controllers/response-questions-controller.js
+++ AnalyzerQuestionsRESTful/src/main/resources/static/js/controllers/response-questions-controller.js
@@ -12,6 +12,7 @@ aqtApp.controller("responseQuestionController", function($scope, userService, qu
 	$scope.checkedQuestionsClicked = [];
 	$scope.question = {};
 	var userStorage = localStorageService.get("aqt-user");
+	var elemetQuestion = $("#body-detail-description");
 	
     $scope.toggleCheck = function (option) {
         if ($scope.checkedQuestionsClicked.indexOf(option) === -1) {
@@ -21,23 +22,27 @@ aqtApp.controller("responseQuestionController", function($scope, userService, qu
         }
         console.log($scope.checkedQuestionsClicked);
     };
-	
 	userService.getById(userStorage.id).$promise.then(
 			function(data) {
 				chosenQuestions = data.chosenQuestionsWrapper.chosenQuestions;
-				$scope.nextQuestion.next();
+				$scope.question = chosenQuestions[0];
+				elemetQuestion.append($scope.question.descritptionHtml);
 			}, function(data) {
 	});
 
-	$scope.nextQuestion = {
-		cont : 0,
-
-		next : function() {
-			var elemetQuestion = $("#body-detail-description");
-			elemetQuestion.empty();
-			$scope.question = chosenQuestions[this.cont];
-			elemetQuestion.append($scope.question.descritptionHtml);
-			this.cont++;
-		}
+	var cont = 0;
+	$scope.next = function() {
+		cont++;
+		
+		elemetQuestion.empty();
+		$scope.question = chosenQuestions[cont];
+		elemetQuestion.append($scope.question.descritptionHtml);
+	};
+	$scope.back = function() {
+		cont--;
+		var elemetQuestion = $("#body-detail-description");
+		elemetQuestion.empty();
+		$scope.question = chosenQuestions[cont];
+		elemetQuestion.append($scope.question.descritptionHtml);
 	}
 });
