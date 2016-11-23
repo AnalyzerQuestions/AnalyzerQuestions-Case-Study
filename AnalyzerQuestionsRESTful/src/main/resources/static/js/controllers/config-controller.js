627571249f3d1795832839cceb2cc2e29eeb54a6
Franck Aragão <fjunior.aragao@gmail.com>
2016-11-23 15:33:05 -0300

    parte de configurações

--- AnalyzerQuestionsRESTful/src/main/resources/static/js/controllers/config-controller.js
+++ AnalyzerQuestionsRESTful/src/main/resources/static/js/controllers/config-controller.js
@@ -1,19 +1,72 @@
 aqtApp.controller("configController", function($scope, aqtConfig, questionService, userService, growl) {
 
 	$scope.config = {};
+	$scope.clicked = {};
+	var isUpdated = false;
+	var isClicked = false;
+	var previusMotive = "";
+
 	aqtConfig.getTime().then(function(response) {
 		$scope.config = response.data;
 	});
-	
-	$scope.update = function(){
-		aqtConfig.updateTime($scope.config).then(function(response) {
-			growl.success("Configurações salvas");
-		});
+
+	$scope.openModal = function(motive, isUp, isCli) {
+		isUpdated = isUp;
+		isClicked = isCli;
+		if (isUp) {
+			previusMotive = motive;
+			$scope.clicked.motive = motive;
+		} else {
+			reset();
+		}
+	};
+
+	$scope.saveMotive = function() {
+		if(isClicked){
+			if (!isUpdated) {
+				saveMotiveClicked($scope.clicked.motive);
+			} else {
+				updateMotiveClicked($scope.clicked.motive);
+			}
+		}else{
+			
+		}
 	};
 	
-	$scope.getSuggestions = function(){
+	var saveMotiveClicked = function(motive){
+		$scope.config.optionsQuestionsClicked.push(motive);
+		$('#optionModal').modal('hide');
+		$scope.update();
+	};
+
+	$scope.removeMotiveClicked = function(motive) {
+		var index = $scope.config.optionsQuestionsClicked.indexOf(motive);
+		$scope.config.optionsQuestionsClicked.splice(index, 1);
+		$scope.update();
+	};
+
+	var updateMotiveClicked = function(motive) {
+		var index = $scope.config.optionsQuestionsClicked.indexOf(previusMotive);
+		$scope.config.optionsQuestionsClicked[index] = motive;
+		$('#optionModal').modal('hide');
+		$scope.update();
+	};
+
+	$scope.update = function() {
+		aqtConfig.updateTime($scope.config).then(function(response) {
+			reset();
+		});
+	};
+
+	$scope.getSuggestions = function() {
 		aqtConfig.getSuggestions().then(function(response) {
 			console.log(response.data);
 		});
+	};
+
+	var reset = function() {
+		$scope.clicked = angular.copy($scope.clicked = {});
+		motive = "";
+		isUpdated = false;
 	};
 });
