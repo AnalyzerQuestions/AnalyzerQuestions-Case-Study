599ff676d1d5adbe51dbc0909b9caada4fa3d08e
Franck Aragão <fjunior.aragao@gmail.com>
2016-11-24 18:52:09 -0300

    translate table

--- AnalyzerQuestionsRESTful/src/main/resources/static/js/config/datatables-config.js
+++ AnalyzerQuestionsRESTful/src/main/resources/static/js/config/datatables-config.js
@@ -1,5 +1,5 @@
 aqtApp.run(function(DTDefaultOptions) {
-
+	
 	DTDefaultOptions.setBootstrapOptions({
 		TableTools : {
 			classes : {
@@ -15,4 +15,30 @@ aqtApp.run(function(DTDefaultOptions) {
 			}
 		}
 	});
+	
+	var language = {
+			"sEmptyTable" : "Nenhum dado encontrado",
+			"sInfo" : "De _START_ até _END_ de _TOTAL_ total",
+			"sInfoEmpty" : "de 0 até 0 de 0 total",
+			"sInfoFiltered" : "(filtreret ud af _MAX_ rækker ialt)",
+			"sInfoPostFix" : "",
+			"sInfoThousands" : ",",
+			"sLengthMenu" : "Mostrando _MENU_ itens",
+			"sLoadingRecords" : "Carregando dados...",
+			"sProcessing" : "Processando...",
+			"sSearch" : "Buscar:",
+			"sZeroRecords" : "Nenhum item encontrado no filtro",
+			"oPaginate" : {
+				"sFirst" : "Primeiro",
+				"sLast" : "Último",
+				"sNext" : "Próximo",
+				"sPrevious" : "Anterior"
+			},
+			"oAria" : {
+				"sSortAscending" : ": Ordenar ascendente",
+				"sSortDescending" : ": Ordenar descrescente"
+			}
+		}
+
+		DTDefaultOptions.setLanguage(language);
 });
