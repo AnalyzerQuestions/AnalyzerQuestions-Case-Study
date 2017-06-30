8eb7c9bc3183a1007ec07ef39011cead27c7bf19
Franck Aragão <fjunior.aragao@gmail.com>
2017-06-30 11:27:15 -0300

    alterando ordem de aparição das mensagens das sugestões

--- src/main/resources/static/js/config/config.js
+++ src/main/resources/static/js/config/config.js
@@ -40,24 +40,35 @@ aqtApp.service('aqtConfig', function($http) {
 	   				tipo : 'SHOT_DESCRIPTION',
 	   				categoria : ['Objetividade']
 
+	   			}, sug = {
+	  		   		  msg : 'Resuma a pergunta a um único problema.',
+//	  	   			  msg : 'Summarize the question in a single problem and/or let question most evident.',
+	  	   			  tipo : 'UNIQUE_QUESTION',
+	  	   			  categoria : ['Objetividade']	   	
 	   			
 	   			}, sug = {
 		   		  msg : 'Seja Objetivo e deixe a pergunta mais evidente possível.',
 //	   			  msg : 'Be objective and let the question most evident.',
 	   			  tipo : 'IS_CLARITY',
-	   			  categoria : ['Clarreza', 'HEADER']
+	   			  categoria : ['Clareza', 'HEADER']
 	   				  
-	   			}, sug = {
-		   		  msg : 'Resuma a pergunta a um único problema.',
-//	   			  msg : 'Summarize the question in a single problem and/or let question most evident.',
-	   			  tipo : 'UNIQUE_QUESTION',
-	   			  categoria : ['Objetividade']
-
 	   			}, sug = {
 	  		   		  msg : 'Resuma a pergunta a um único problema e o deixe mais evidente possível.',
 	  	   			  tipo : 'EVIDENT_QUESTION',
 	  	   			  categoria : ['Clareza']
 	   				  
+	   			}, sug = {
+	  		   		  msg : 'O Título da pergunta parece não está coerente com a descrição.',
+//	  	   			  msg : 'The title of the question is not consistent with the description.',
+	  	   			  tipo : 'COERENCY_BODY_WITH_TITLE',
+	  	   			  categoria : ['Clareza']
+	   			
+	   			}, sug = {
+	  		   		  msg : 'Seja educado',
+//	  	   			  msg : 'Be polite, use proper language and add a greeting.',
+	  	   			  tipo : 'BE_EDUCADED',
+	  	   			  categoria : ['Ser educado', 'HEADER']	   			
+	   			
 	   			}, sug = {
 		   		  msg : 'Use a norma culta da língua.',
 //	   			  msg : 'Use proper language.',
@@ -69,7 +80,19 @@ aqtApp.service('aqtConfig', function($http) {
 //	   			  msg : 'Add some kind of thanks at the end of the question.',
 	   			  tipo : 'GREAT',
 	   			  categoria : ['Ser educado']
+	   			
+	   			}, sug = {
+	  		   		  msg : 'Evite criar perguntas sobre trabalhos acadêmicos.',
+//	  	   			  msg : 'Avoid creating homework question.',
+	  	   			  tipo : 'HOME_WORK_QUESTION',
+	  	   			  categoria : ['Ser educado']	   			
 	   				 
+	   			}, sug = {
+	  		    	  msg : 'Seja mais objetivo e claro na formulação da pergunta.',
+//	  	   			  msg : 'Be more objective and course on the question',
+	  	   			  tipo : 'UNDESTANDABLE_DESCRTION',
+	  	   			  categoria : ['Descrição bem definida', 'HEADER']	   			
+	   			
 	   			}, sug = {
 		   		  msg : 'Adicione um trecho de código ou outro de tipo de exemplo na pergunta.',
 //	   			  msg : 'Add a snippet of code or another kind of example.',
@@ -82,38 +105,13 @@ aqtApp.service('aqtConfig', function($http) {
 	  	   			  categoria : ['Descrição bem definida']	   			
 	   				  
 	   			}, sug = {
-		   		  msg : 'Seja educado',
-//	   			  msg : 'Be polite, use proper language and add a greeting.',
-	   			  tipo : 'BE_EDUCADED',
-	   			  categoria : ['Ser educado', 'HEADER']
-	   			
-	   			}, sug = {
-		   		  msg : 'Evite criar perguntas sobre trabalhos acadêmicos.',
-//	   			  msg : 'Avoid creating homework question.',
-	   			  tipo : 'HOME_WORK_QUESTION',
-	   			  categoria : ['Ser educado']
-	   			
-	   			}, sug = {
-		    	  msg : 'Seja mais objetivo e claro na formulação da pergunta.',
-//	   			  msg : 'Be more objective and course on the question',
-	   			  tipo : 'UNDESTANDABLE_DESCRTION',
-	   			  categoria : ['Descrição bem definida', 'HEADER']
-
-	   			}, sug = {
 	  		    	  msg : 'Evite adicionar muito código a pergunta.',
 	  	   			  tipo : 'IS_OBJECTIVE_MUCH_CODE',
 	  	   			  categoria : ['Descrição bem definida']	   			
 	   				  
-	   			}, sug = {
-		   		  msg : 'O Título da pergunta parece não está coerente com a descrição.',
-//	   			  msg : 'The title of the question is not consistent with the description.',
-	   			  tipo : 'COERENCY_BODY_WITH_TITLE',
-	   			  categoria : ['Clareza']
 	   			}
 	   		];
 
-		
 		return suggestions;
 	};
-
 });
