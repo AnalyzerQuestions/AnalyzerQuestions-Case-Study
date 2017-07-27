97fc2719749663957d6f0c5f8bda023689bbb36d
Franck Aragão <fjunior.aragao@gmail.com>
2017-07-27 13:35:24 -0300

    alterando posição de linha em detalhes de respondente

--- src/main/resources/static/js/config/config.js
+++ src/main/resources/static/js/config/config.js
@@ -29,7 +29,7 @@ aqtApp.service('aqtConfig', function($http) {
 		suggestions = [ 
 		               
 	   			sug = {
-		   			msg : 'Seja Objetivo',
+		   			msg : 'Seja objetivo na descrição da pergunta.',
 //	   				msg : 'Describe your problem in a way simple and direct.',
 	   				tipo : 'IS_OBJECTIVE',
 	   				categoria :['Objetividade', 'HEADER']
@@ -41,42 +41,42 @@ aqtApp.service('aqtConfig', function($http) {
 	   				categoria : ['Objetividade']
 
 	   			}, sug = {
-	  		   		  msg : 'Resuma a pergunta a um único problema.',
+	  		   		  msg : 'Atenha a pergunta a um único problema.',
 //	  	   			  msg : 'Summarize the question in a single problem and/or let question most evident.',
 	  	   			  tipo : 'UNIQUE_QUESTION',
 	  	   			  categoria : ['Objetividade']	   	
 	   			
 	   			}, sug = {
-		   		  msg : 'Seja Objetivo e deixe a pergunta mais evidente possível.',
+		   		  msg : 'Deixe o problema o mais evidente possível na descrição da pergunta.',
 //	   			  msg : 'Be objective and let the question most evident.',
 	   			  tipo : 'IS_CLARITY',
 	   			  categoria : ['Clareza', 'HEADER']
 	   				  
 	   			}, sug = {
-	  		   		  msg : 'Resuma a pergunta a um único problema e o deixe mais evidente possível.',
+	  		   		  msg : 'Resuma a pergunta a um único problema e deixe-o mais evidente possível.',
 	  	   			  tipo : 'EVIDENT_QUESTION',
 	  	   			  categoria : ['Clareza']
 	   				  
 	   			}, sug = {
-	  		   		  msg : 'O Título da pergunta parece não está coerente com a descrição.',
+	  		   		  msg : 'O título da pergunta parece não estar coerente com a descrição.',
 //	  	   			  msg : 'The title of the question is not consistent with the description.',
 	  	   			  tipo : 'COERENCY_BODY_WITH_TITLE',
 	  	   			  categoria : ['Clareza']
 	   			
 	   			}, sug = {
-	  		   		  msg : 'Seja educado',
+	  		   		  msg : 'Seja educado.',
 //	  	   			  msg : 'Be polite, use proper language and add a greeting.',
 	  	   			  tipo : 'BE_EDUCADED',
 	  	   			  categoria : ['Ser educado', 'HEADER']	   			
 	   			
 	   			}, sug = {
-		   		  msg : 'Use a norma culta da língua.',
+		   		  msg : 'Use a norma culta da língua portuguesa.',
 //	   			  msg : 'Use proper language.',
 	   			  tipo : 'USING_PROPER_LANGUAGE',
 	   			  categoria : ['Ser educado']
 	   					  
 	   			}, sug = {
-		   	      msg : 'Adicione algum tipo de agradecimento ao final de sua pergunta.',
+		   	      msg : 'Adicione algum tipo de agradecimento ao final da pergunta.',
 //	   			  msg : 'Add some kind of thanks at the end of the question.',
 	   			  tipo : 'GREAT',
 	   			  categoria : ['Ser educado']
@@ -88,24 +88,24 @@ aqtApp.service('aqtConfig', function($http) {
 	  	   			  categoria : ['Ser educado']	   			
 	   				 
 	   			}, sug = {
-	  		    	  msg : 'Seja mais objetivo e claro na formulação da pergunta.',
+	  		    	  msg : 'Escreva uma boa descrição para o seu problema, seja objetivo e claro.',
 //	  	   			  msg : 'Be more objective and course on the question',
 	  	   			  tipo : 'UNDESTANDABLE_DESCRTION',
 	  	   			  categoria : ['Descrição bem definida', 'HEADER']	   			
 	   			
 	   			}, sug = {
-		   		  msg : 'Adicione um trecho de código ou outro de tipo de exemplo na pergunta.',
+		   		  msg : 'Adicione um trecho de código ou outro de tipo de exemplo na descrição da pergunta.',
 //	   			  msg : 'Add a snippet of code or another kind of example.',
 	   			  tipo : 'SHOW_EXAMPLE',
 	   			  categoria : ['Descrição bem definida']
 	   			
 	   			}, sug = {
-	  		   		  msg : 'Evite adicionar links a pergunta sem especificar o seu conteúdo.',
+	  		   		  msg : 'Evite adicionar links a pergunta sem especificar do que se trata.',
 	  	   			  tipo : 'COMBINATE_URL',
 	  	   			  categoria : ['Descrição bem definida']	   			
 	   				  
 	   			}, sug = {
-	  		    	  msg : 'Evite adicionar muito código a pergunta.',
+	  		    	  msg : 'Evite adicionar um trecho de código muito longo na descrição da pergunta.',
 	  	   			  tipo : 'IS_OBJECTIVE_MUCH_CODE',
 	  	   			  categoria : ['Descrição bem definida']	   			
 	   				  
